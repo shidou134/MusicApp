693bbb83c5051204ed10c676c5f863d7ba7e0a16
shidou134 <bigboy901902@gmail.com>
2024-02-23 10:27:54 +0700

    update ui

--- app/src/main/java/com/example/musicapp/ui/SongViewModel.kt
+++ app/src/main/java/com/example/musicapp/ui/SongViewModel.kt
@@ -10,18 +10,22 @@ import com.example.musicapp.model.SongUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SongViewModel() : ViewModel() {
 
     private val _uiState = MutableStateFlow(SongUiState())
     var uiState: StateFlow<SongUiState> = _uiState.asStateFlow()
 
+    private lateinit var player:ExoPlayer
+
     fun setMusicExoPlayer(context:Context){
 //    val context = LocalContext.current
-        val player = ExoPlayer.Builder(context).build()
+        player = ExoPlayer.Builder(context).build()
         val mediaItem = MediaItem.fromUri(_uiState.value.currentSong!!.url)
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
     }
+   
 }
