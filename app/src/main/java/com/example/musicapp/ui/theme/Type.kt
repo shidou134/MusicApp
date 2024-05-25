0aed664009c1d0f5d5a18809a06551ca6135b646
shidou134 <bigboy901902@gmail.com>
2024-05-25 22:09:24 +0700

    update font

--- app/src/main/java/com/example/musicapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/musicapp/ui/theme/Type.kt
@@ -9,26 +9,26 @@ import androidx.compose.ui.unit.sp
 import com.example.musicapp.R
 
 // Set of Material typography styles to start with
-val montserrat = FontFamily(
+val inter = FontFamily(
     Font(R.font.inter_bold, FontWeight.Bold),
     Font(R.font.inter_regular)
 
 )
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = montserrat,
+        fontFamily = inter,
         fontWeight = FontWeight.Normal,
         fontSize = 20.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = montserrat,
+        fontFamily = inter,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = montserrat,
+        fontFamily = inter,
         fontWeight = FontWeight.Bold,
         fontSize = 50.sp
     )
